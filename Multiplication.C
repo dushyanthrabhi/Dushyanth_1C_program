784db164003e1da38d561ea94218c078c70d857e
dushyanthrabhi <dushyanthrabhi159@gmail.com>
2024-11-26 15:56:19 +0530

    Update Multiplication.C

--- Multiplication.C
+++ Multiplication.C
@@ -1,1 +1,10 @@
-https://replit.com/@dushyanthrabhi1/MultiplicationC#main.c
+#include <stdio.h>
+int main() {
+  int num;
+  printf("Enter a number:");
+  scanf("%d",&num);
+  for(int i=1;i<=10;i++){
+    printf("%d×%d=%d\n",num,i,num*i);
+  }
+  return 0;
+}
